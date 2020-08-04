b72137a87b56d821f10ad32a85607234aee919df
kevintsai <tsaimengxd@gmail.com>
2020-08-04 09:31:40 -0600

    add counting feature

--- sortLabeljson.py
+++ sortLabeljson.py
@@ -3,13 +3,13 @@ import glob
 import json
 from IPython import embed
 
-imagesdir = "./cheese5-bad"
+imagesdir = "./cheese5-good"
 labelsdir = "./defect_labels"
 
-classes = {"None", "Double Sealed", "Two Trays", "Sideways Tray", "No Seal", "Other Defect"}
+classes = {"None":0, "Double Sealed":0, "Two Trays":0, "Sideways Tray":0, "No Seal":0, "Other Defect":0}
 labels_list = glob.glob(os.path.join(labelsdir, "*.json"))
 
-
+cnt = 0
 for labelsjson in labels_list:
     with open(labelsjson) as json_file:
         data = json.load(json_file)
@@ -26,23 +26,27 @@ for labelsjson in labels_list:
         if fn[-3:] != "png":
             continue
         else:
+            cnt += 1
             filedir = os.path.join(imagesdir, fn)
             if data[fn]:            
                 for c in data[fn]:
                     labeldir = os.path.join(imagesdir, c, fn)     
                     try:
-                        os.link(filedir, labeldir)                
+                        os.link(filedir, labeldir)    
+                        classes[c] += 1            
                     except:
                         pass
             else:
                 labeldir = os.path.join(imagesdir, "None", fn)
                 try:
-                    os.link(filedir, labeldir)                
+                    os.link(filedir, labeldir)    
+                    classes["None"] += 1            
                 except:
                     pass
 
-
-
+print("Total images:", cnt)
+for c in classes:
+    print(c,":",classes[c])
 
 
 
