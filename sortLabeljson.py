aa4334b23a6c2da99501461762cdd25559cd6031
kevintsai <tsaimengxd@gmail.com>
2020-08-04 09:07:43 -0600

    add new feature

--- sortLabeljson.py
+++ sortLabeljson.py
@@ -6,33 +6,40 @@ from IPython import embed
 imagesdir = "./cheese5-bad"
 labelsdir = "./defect_labels"
 
-classes = {"Double Sealed", "Two Trays", "Sideways Tray", "No Seal", "Other Defect"}
+classes = {"None", "Double Sealed", "Two Trays", "Sideways Tray", "No Seal", "Other Defect"}
 labels_list = glob.glob(os.path.join(labelsdir, "*.json"))
 
 for labelsjson in labels_list:
-    with open(labelsjson) as json_file:
-        data = json.load(json_file)
+
+with open(labelsdir) as json_file:
+    data = json.load(json_file)
 
 
-    for c in classes:    
-        try:
-            os.mkdir(os.path.join(imagesdir,c))
-        except:
-            os.system("rm -rf \""+ os.path.join(imagesdir,c)+"\"")
-            os.mkdir(os.path.join(imagesdir,c))
+for c in classes:    
+    try:
+        os.mkdir(os.path.join(imagesdir,c))
+    except:
+        os.system("rm -rf \""+ os.path.join(imagesdir,c)+"\"")
+        os.mkdir(os.path.join(imagesdir,c))
 
-    for fn in data:
-        if fn[-3:] != "png":
-            continue
+for fn in data:
+    if fn[-3:] != "png":
+        continue
+    else:
+        filedir = os.path.join(imagesdir, fn)
+        if data[fn]:            
+            for c in data[fn]:
+                labeldir = os.path.join(imagesdir, c, fn)     
+                try:
+                    os.link(filedir, labeldir)                
+                except:
+                    pass
         else:
-            if data[fn]:
-                filedir = os.path.join(imagesdir, fn)
-                for c in data[fn]:
-                    labeldir = os.path.join(imagesdir, c, fn)     
-                    try:
-                        os.link(filedir, labeldir)
-                    except:
-                        pass
+            labeldir = os.path.join(imagesdir, "None", fn)
+            try:
+                os.link(filedir, labeldir)                
+            except:
+                pass
 
 
 
